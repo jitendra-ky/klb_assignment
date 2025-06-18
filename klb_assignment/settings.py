66127407c1f45ffa08d5d05c3059fbfbd64ebd60
Jitendra Kumar <jk69854@gmail.com>
2025-06-18 15:57:51 +0530

    Add JWT authentication support.

--- klb_assignment/settings.py
+++ klb_assignment/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",  # Django REST Framework for API development
     "users",  # Custom user app
 ]
 
@@ -48,6 +49,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 ROOT_URLCONF = "klb_assignment.urls"
 
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 """URL configuration for the users app."""
 
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="users_index"),
+
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
